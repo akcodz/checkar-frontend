cb3d7ee997b6664a96efc43c5c6dd73c5dbb79f8
Akash <116895304+akcodz@users.noreply.github.com>
2026-05-04 12:08:50 +0530

    Rename TopBar import to Topbar

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import Sidebar from "./Sidebar"
-import TopBar from "./TopBar"
+import TopBar from "./Topbar"
 
 const DashboardLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
